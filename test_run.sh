a4b96774ad8c845857a8319c1d38a4e6729be218
Alexander <aburlaka@rambler.ru>
2025-09-02 21:24:50 +0100

    used --baseURL --browserName --browserVersion

--- test_run.sh
+++ test_run.sh
@@ -5,11 +5,13 @@
 url=""
 url_regexp="^https?://([^/]+)"
 browser=""
+version=""
+version_regexp="^[1-9]"
 comand="mvn clean test"
 
 while [[ $# -gt 0 ]]; do
 	case $1 in
-		--url)
+		--baseURL)
 			url=$2
 			if [[ $url =~ $url_regexp ]]; then
 				shift
@@ -19,15 +21,25 @@ while [[ $# -gt 0 ]]; do
 				exit 1
 			fi
 			;;
-		--browser)
+		--browserName)
 			browser=$2
 			browser=${browser,,}
-			if [[ ($browser != "chrome") && ($browser != "firefox") && ($browser != "edge") ]]; then
+			if [[ ($browser != "chrome") && ($browser != "firefox") && ($browser != "edge") && ($browser != "remotechrome" ) ]]; then
 				echo "Error: browser $browser is not supported"
                                 exit 1
 			fi
 			shift
 			shift
+			;;
+		--browserVersion)
+			version=$2
+			if [[ $version =~ $version_regexp ]]; then
+				shift
+				shift
+			else
+				echo "Error: browserVersion must begin with number"
+				exit 1
+			fi
 			;;
 		*)
 			shift
@@ -37,11 +49,15 @@ while [[ $# -gt 0 ]]; do
 done
 
 if [[ $browser != "" ]]; then
-	comand=$comand" -Dbrowser="$browser
+	comand=$comand" -DbrowserName="$browser
+fi
+
+if [[ $version != "" ]]; then
+	comand=$comand" -DbrowserVersion="$version
 fi
 
 if [[ $url != "" ]]; then
-	comand=$comand" -Dbase.url="$url
+	comand=$comand" -DbaseURL="$url
 fi
 
 echo $comand
